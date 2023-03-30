097ded12cf8f3f92c55d111146be026c8e97ba36
kjhchief <kjhchief@naver.com>
2023-03-30 16:29:37 +0900

    first

---
+++ src/main/java/com/runningmate/ProjectRunningmateApplication.java
@@ -0,0 +1,13 @@
+package com.runningmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRunningmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRunningmateApplication.class, args);
+	}
+
+}
