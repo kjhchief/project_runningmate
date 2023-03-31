f6f56965c8ebbc53386cac366d2c376145c67fdd
kjhchief <kjhchief@naver.com>
2023-03-31 12:53:32 +0900

    타임리프

---
+++ src/main/java/com/runningmate/web/main/controller/ThymeleafController.java
@@ -0,0 +1,28 @@
+package com.runningmate.web.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class ThymeleafController {
+
+	
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index_example";
+	}
+	
+	@GetMapping("/main")
+	public String main() {
+		return "main";
+	}
+	
+	
+	
+
+}
