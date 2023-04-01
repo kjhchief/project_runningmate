bd3431af8b983013adb49ebbb52811466296a524
kjhchief <kjhchief@naver.com>
2023-04-01 17:24:45 +0900

    디비에 모임만들기 데이터 저장 성공. 날짜형식 문제만..

--- src/main/java/com/runningmate/web/crew/controller/CrewController.java
+++ src/main/java/com/runningmate/web/crew/controller/CrewController.java
@@ -2,6 +2,7 @@ package com.runningmate.web.crew.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,15 +10,20 @@ import com.runningmate.domain.crew.dto.Crew;
 import com.runningmate.domain.crew.service.CrewService;
 
 @Controller
-@RequestMapping("/crew")
+@RequestMapping
 public class CrewController {
 	
 	@Autowired 
 	private CrewService crewService;
 	
-	@PostMapping("/createcrew")
+	@PostMapping("/main")
 	public void createCrew(Crew crew) {
 		crewService.createCrew(crew);
+	}
+	
+	@GetMapping("/createcrew")
+	public String createcrew() {
+		return "createcrew";
 	}
 	
 	
