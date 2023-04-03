98b7e731339ef808e75cef2115dbc54cde727e47
kjhchief <kjhchief@naver.com>
2023-04-03 17:30:33 +0900

    ㅎ

--- src/test/java/com/runningmate/PhotoUploadTest.java
+++ src/test/java/com/runningmate/PhotoUploadTest.java
@@ -1,7 +1,10 @@
 package com.runningmate;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +36,43 @@ public class PhotoUploadTest {
 	
 	@Autowired
 	private PhotoStore fileStore;
+	
+	public String getFullPath(String filename) {
+		return location + filename;
+	}
+
+	public List<UploadPhoto> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+		List<UploadPhoto> storeFileResult = new ArrayList<UploadPhoto>();
+		for (MultipartFile multipartFile : multipartFiles) {
+			if (!multipartFile.isEmpty()) {
+				// 업로드 파일 저장
+				UploadPhoto uploadFile = storeFile(multipartFile);
+				storeFileResult.add(uploadFile);
+			}
+		}
+		return storeFileResult;
+	}
+
+	public UploadPhoto storeFile(MultipartFile multipartFile) throws IOException {
+		if (multipartFile == null || multipartFile.isEmpty()) {
+			return null;
+		}
+		String originalFilename = multipartFile.getOriginalFilename();
+		String storeFileName = createStoreFileName(originalFilename);
+		multipartFile.transferTo(new File(getFullPath(storeFileName)));
+		return new UploadPhoto(originalFilename, storeFileName);
+	}
+
+	private String createStoreFileName(String originalFilename) {
+		String ext = extractExt(originalFilename);
+		String uuid = UUID.randomUUID().toString();
+		return uuid + "." + ext;
+	}
+
+	private String extractExt(String originalFilename) {
+		int pos = originalFilename.lastIndexOf(".");
+		return originalFilename.substring(pos + 1);
+	}
 	
 	@Test
 	void photoUpTest() throws IOException {
