609933002434a2486b121d8bbd124468ee993b01
zino416 <thdx416@naver.com>
2023-04-07 09:22:25 +0900

    newwwwwwwwwwwwwwwwww

--- src/main/java/com/runningmate/web/mate/controller/MateController.java
+++ src/main/java/com/runningmate/web/mate/controller/MateController.java
@@ -1,9 +1,13 @@
 package com.runningmate.web.mate.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
-@SessionAttributes("mate")
+//@SessionAttributes("mate")
 @RequestMapping("/mate")
 public class MateController {
 
@@ -51,7 +55,7 @@ public class MateController {
 		}
 	}
 
-	// 회원가입 처리에 대한 메소드
+	// 회원가입 처리 메소드
 	@PostMapping
 	public String register(@ModelAttribute("mate") Mate mate) {
 		mate.setBirthdate();
@@ -59,19 +63,40 @@ public class MateController {
 		mate.setPhoneNumber();
 		log.info("mate {}", mate);
 		mateService.create(mate);
-		return "redirect:/mate/result";
+		return "redirect:/mate/main";
 	}
-/*
+	//로그인 처리 메소드
 	@PostMapping("/login-check")
 	@ResponseBody
-	public String Login(@ModelAttribute("mate") Mate mate) {
-		mateService.getLoginInfo(mate);
-		return "mate";
+	public String Login(@ModelAttribute("mate") Mate mate, 
+			HttpSession session, HttpServletResponse response, 
+			@CookieValue(value = "saveEmail", required = false) String saveEmail, 
+			Model model) {
+		
+		// required = false 파라미터가 필수적이지 않음을 명시
+		if(mateService.getLoginInfo(mate) != null) {
+			session.setAttribute("mate", mateService.getLoginInfo(mate));
+			
+			if (saveEmail != null) {
+				
+				log.info("쿠키{}", saveEmail);
+				Cookie cookie = new Cookie("savedEmail", mate.getEmail());
+				cookie.setMaxAge(60 * 60 * 1 * 1); // 1시간
+				cookie.setPath("/");
+				response.addCookie(cookie);
+				
+				 model.addAttribute("savedEmail", saveEmail);
+			}
+			
+			return "success";
+		}else {
+			return "failure";
+		}
 		
 	}
-	*/
+	
 	// 회원가입결과에 대한 메소드
-	@GetMapping("/result")
+	@GetMapping("/main")
 	public String registerResult() {
 		return "main";
 	}
--- src/main/resources/static/js/mate/login.js
+++ src/main/resources/static/js/mate/login.js
@@ -1,30 +1,64 @@
 const form = document.getElementById('loginForm');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
 form.addEventListener('submit', (event) =>{
+    //const checkedSave = document.querySelector('#saveEmail');
+
 	 // 폼 유효성 검사
-    if (!form.checkValidity()) {
-		
+    if ((!form.checkValidity())){
       // 검증 실패시 폼 제출 안되게 비활성화
       event.preventDefault();
       // 클릭 이벤트가 상위 요소로 전파되지 않도록 중단시킴
       event.stopPropagation();
-    }
-
     form.classList.add('was-validated');
+    }else{
+		sendEmailPassword(email.value, password.value);
+	}
+    
+    //if(checkedSave.checked){}	
 });
 
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
 
 function sendEmailPassword(email, password){
 	let option = {
 		method: "post",
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded"
+			
 		},
 		body: `email=${email}&password=${password}` //서버로 전송할 데이터
 	};
 	fetch("/mate/login-check", option)
-		.then(respose => respose.json())
+		.then(respose => respose.text())
 		.then(result => loginResultMessage(result))
 		.catch(error => console.log(error));
 }
+
+function loginResultMessage(result){
+	if(result === 'success'){
+		location.href='/mate/main';
+	}else if(result === 'failure'){
+		alert("이메일, 비밀번호를 다시 입력 해주세요.");
+	}
+	
+}
+
+/*
+// "saveEmail" 쿠키 값 읽어오기
+var savedEmail = getCookie("saveEmail");
+
+// "이메일" 입력란에 쿠키 값 채우기
+if (savedEmail) {
+  document.getElementById("email").value = savedEmail;
+}
+
+// 쿠키 값을 읽어오는 함수 구현
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+}
+*/
